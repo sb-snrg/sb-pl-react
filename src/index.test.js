8e7b381d4a023639e191cc707ec1ea1ac514e89e
snrg <platform.synergy@gmail.com>
2017-06-26 02:59:45 +0000

    1-Environment-Setup

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jaures on 26/06/2017.
+ */
+import expect from 'expect';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).toEqual(true);
+  });
+});
