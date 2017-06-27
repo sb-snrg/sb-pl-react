fa5e79ab4cb200a4408caf8fd20a010c36e08225
snrg <platform.synergy@gmail.com>
2017-06-27 10:13:30 +0000

    5- Setting Asynchronous in redux (Redux Thunk), Setting Mock Api, Setting page load course with redux thunk

--- src/index.js
+++ src/index.js
@@ -6,10 +6,16 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
+
+/*dispatch qui prend en paramètre une action et exécute le reducer qui va,
+  à son tour, mettre à jour le store avec un nouvel état*/
+
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
