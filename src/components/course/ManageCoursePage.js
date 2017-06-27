4990fa30bb15107a0a5eafc13f19a2c58187d1dd
snrg <platform.synergy@gmail.com>
2017-06-27 19:23:36 +0000

    11- Async Writes in Redux : Populate Form via mapStateToProps, Update State via componentWillReceiveProps

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -14,11 +14,19 @@ class ManageCoursePage extends React.Component {
     this.updateCourseState=this.updateCourseState.bind(this);
     this.saveCourse = this.saveCourse.bind(this);
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.course.id != nextProps.course.id) {
+      // Necessary to populate form when existing course is loaded directly.
+      this.setState({course: Object.assign({}, nextProps.course)});
+    }
+  }
+
   /** Create Form Change Handler **/
   updateCourseState(event) {
     const field = event.target.name;
-    let course = this.state.course;
- //   let course = Object.assign ({},this.state.course);
+ //   let course = this.state.course;
+    let course = Object.assign ({},this.state.course);
     course[field] = event.target.value;
     return this.setState({course: course});
   }
@@ -53,8 +61,20 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 };
 
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id == id);
+  if (course.length) return course[0]; //since filter returns an array, have to grab the first.
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id; // from the path `/course/:id`
+
   let course = {id: '', watchHref:'', title:'', authorId:'', length:'', category:''};
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
 
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
